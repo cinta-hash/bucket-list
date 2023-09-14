edbcd7bbd46da6109c3aeee8a1f360d5dba4b2d4
jacinta mwangi <jacinta.mwangi.student@moringaschool.com>
2023-09-14 13:21:53 +0300

    added function addItem

--- app.js
+++ app.js
@@ -11,4 +11,25 @@ addInputBtn.addEventListener('click', addItem)
 function addItem(e){
     e.preventDefault();
 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todoDiv");
+
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-item");
+
+    todoDiv.appendChild(newTodo);
+    
+    const checkBtn = document.createElement("button");
+    checkBtn.textContent = "check"
+    checkBtn.classList.add("material-symbols-outlined");
+    
+    todoDiv.appendChild(checkBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete"
+    deleteBtn.classList.add("material-symbols-outlined");
+
+    todoDiv.appendChild(deleteBtn);
+
+    todoList.appendChild(todoDiv);
 }
