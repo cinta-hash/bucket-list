3527224a1bc1ba909c7c489d4242d7c003fb24ca
jacinta mwangi <jacinta.mwangi.student@moringaschool.com>
2023-09-14 12:46:12 +0300

    added functionality

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //Selectors
+const input = document.querySelector(".input-area");
+const addInputBtn = document.querySelector("#add-item");
+const todoList = document.querySelector(".to-do-list");
 
 
 //EventListeners
+addInputBtn.addEventListener('click', addItem)
 
 //Functions
+function addItem(e){
+    e.preventDefault();
+
+}
